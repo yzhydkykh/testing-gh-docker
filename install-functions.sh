bcd5913e7e6cb5be66b020fcb6e66bd64ffe54ec
yzhydkykh <yevgen1zhydkykh@gmail.com>
2023-06-13 16:05:16 +0100

    test

--- install-functions.sh
+++ install-functions.sh
@@ -8,6 +8,7 @@ fi
 echo "HELLO WORLD!"
 ls
 yarn install && yarn build
-cd functions
-yarn install && yarn build
+cd packages/app/functions
+yarn install
+cd ../../..
 firebase deploy --token ${FIREBASE_TOKEN} --only functions
