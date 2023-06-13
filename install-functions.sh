046b7fca06903b3a9328b0fb17d22699bebd0175
yzhydkykh <yevgen1zhydkykh@gmail.com>
2023-06-13 15:16:06 +0100

    test

--- install-functions.sh
+++ install-functions.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 
-firebase deploy --only functions
+if [ -z "${FIREBASE_TOKEN}" ]; then
+    echo "FIREBASE_TOKEN is missing"
+    exit 1
+fi
+
+firebase deploy --token ${FIREBASE_TOKEN} \ --only functions
